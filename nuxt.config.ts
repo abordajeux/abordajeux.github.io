e3aebc0839c109acaddc619ad663acc53d173bcf
Martin Künzi <Martin.Kuenzi@swisstopo.ch>
2026-05-01 19:23:30 +0200

    adding things for auto-deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,14 @@
 import { resolve } from 'path'
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
+    ssr: false, // ensures full static SPA behavior (optional but safest)
+
+  nitro: {
+    preset: 'github_pages'
+  },
+
+  app: {
+    baseURL: '/abordajeux.github.io/',}, // IMPORTANT (remove later if using custom domain)
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@pinia/nuxt'],
