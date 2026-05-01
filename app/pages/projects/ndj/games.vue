ae1bff2a494b1e8506e7af1ba3b4c37e97c9e75b
Martin Künzi <Martin.Kuenzi@swisstopo.ch>
2026-05-01 19:06:53 +0200

    adding placeholders for first project

---
+++ app/pages/projects/ndj/games.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-[400px] flex items-center justify-center">
+    <h1 class="text-4xl font-bold text-primary">
+      PLACEHOLDER
+    </h1>
+  </div>
+</template>
---
+++ app/pages/projects/ndj/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="min-h-[400px] flex items-center justify-center">
+    <h1 class="text-4xl font-bold text-primary">
+      PLACEHOLDER
+    </h1>
+    <div class="text-neutral"> We should have this in another way</div>
+  </div>
+</template>
